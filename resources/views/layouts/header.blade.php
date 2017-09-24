8e48b52334f6e0cb1576498c4f99557fd2e6d714
nihaoZWP <719899730@qq.com>
2017-09-24 15:36:04 +0800

    name

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,7 +21,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a href="{{ route('post.index') }}"
-                   class="navbar-brand">{{ $author or 'Blog' }}</a>
+                   class="navbar-brand">Blog</a>
             </div>
             <div class="collapse navbar-collapse fix-top" id="blog-navbar-collapse">
                 <ul class="nav navbar-nav">
