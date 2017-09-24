44cb6799a14d709dec203a15b9b3b73ad2c62209
nihaoZWP <719899730@qq.com>
2017-09-24 20:00:00 +0800

    内容调整

--- config/social.php
+++ config/social.php
@@ -7,21 +7,21 @@ return [
  * Date: 2016/8/21
  * Time: 22:47
  */
-//    'facebook' => [
-//        'url'  => 'https://www.facebook.com/lufficc',
-//        'icon'   => 'facebook',
-//    ],
+    'github'  => [
+        'url' =>'https://github.com/zhangweipengphp',
+        'icon'   => 'github',
+    ],
+    'facebook' => [
+        'url'  => 'https://www.facebook.com/weipeng.zhang.5243',
+        'icon'   => 'facebook',
+    ],
 //    'twitter'  => [
 //        'url'  => 'https://twitter.com/lufficc',
 //        'icon'   => 'twitter',
 //    ],
-//    'weibo'    => [
-//        'url'  => 'http://weibo.com/u/2363498941',
-//        'icon'   => 'weibo',
-//    ],
-    'github'  => [
-        'url' =>'https://github.com/zhangweipengphp',
-        'icon'   => 'github',
+    'weibo'    => [
+        'url'  => 'https://weibo.com/u/6288484458',
+        'icon'   => 'weibo',
     ],
 
 ];
